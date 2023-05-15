b5d2aa12434bc5d53800a05bcd0ec330ee0f418b
taehyeon83 <wxg5483@naver.com>
2023-05-15 17:06:31 +0900

    test

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Item from "./components/Item";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      test
+      <Item />
       <Footer />
     </div>
   );
