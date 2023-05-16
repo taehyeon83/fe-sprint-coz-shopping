d7b21ac51f95c7a9d93477bd600d8508b6860572
taehyeon83 <wxg5483@naver.com>
2023-05-16 11:26:16 +0900

    Item 구현

--- shopping/src/components/Item.js
+++ shopping/src/components/Item.js
@@ -1,5 +1,27 @@
-function Item() {
-  return <div className="item"></div>;
+import "../styles/item.css";
+
+function Item({products}) {
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img className="image" src={product.image_url || product.brand_image_url}></img>
+          <div className="topLine">
+            <div className="leftText">
+              <div className="productTitle">{product.type === "Category" ? "#" + product.title : product.title || product.brand_name}</div>
+            </div>
+            <div className="rightText">
+              {product.type === "Product" ? product.discountPercentage + "%" : null}
+              {product.type === "Brand" ? "관심고객수" : null}
+            </div>
+          </div>
+          <div className="bottomLine">
+            <div>{product.price || product.follower}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Item;
