b501b75ccd52580d812a5e70897e7363493c2462
taehyeon83 <wxg5483@naver.com>
2023-05-17 11:25:20 +0900

    alt 추가

--- shopping/src/components/Item.js
+++ shopping/src/components/Item.js
@@ -3,24 +3,24 @@ import "../styles/item.css";
 function Item({products}) {
   return (
     <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <img className="image" src={product.image_url || product.brand_image_url}></img>
-          <div className="topLine">
-            <div className="leftText">
-              <div className="productTitle">{product.type === "Category" ? "#" + product.title : product.title || product.brand_name}</div>
+      <div className="product">
+        {products.map((product) => (
+          <div key={product.id}>
+            <img className="image" alt="제품이미지" src={product.image_url || product.brand_image_url}></img>
+            <div className="topLine">
+              <div className="leftText">
+                <div className="productTitle">{product.type === "Category" ? "#" + product.title : product.title || product.brand_name}</div>
+              </div>
+              <div className="rightText">
+                {product.type === "Product" ? product.discountPercentage + "%" : null}
+                {product.type === "Brand" ? "관심고객수" : null}
+              </div>
             </div>
-            <div className="rightText">
-              {product.type === "Product" ? product.discountPercentage + "%" : null}
-              {product.type === "Brand" ? "관심고객수" : null}
-            </div>
+            <div className="priceAndfollwer">{product.price || product.follower}</div>
+            <div className="subTitle">{product.sub_title}</div>
           </div>
-          <div className="bottomLine">
-            <div>{product.price || product.follower}</div>
-            <div>{product.sub_title}</div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
