08db3ece694286880c36a60a1384b17cc2feed39
taehyeon83 <wxg5483@naver.com>
2023-05-25 13:14:02 +0900

    Refactoring

--- shopping/src/components/Item.js
+++ shopping/src/components/Item.js
@@ -1,46 +1,85 @@
-import "../styles/item.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faStar} from "@fortawesome/free-solid-svg-icons";
-import React, {useState} from "react";
+import React from "react";
+import "../styles/Item.css";
+import {useSelector, useDispatch} from "react-redux";
+import {addToBookmark, removeFromBookmark} from "../store";
+import {useState} from "react";
 
-function Item({products}) {
-  const [bookmarkedItems, setBookmarkedItems] = useState([]);
+function Item(props) {
+  const {id} = props;
+  const bookmarkedItems = JSON.parse(localStorage.getItem("bookmarkedItems")) || [];
+  const [modalOpen, setModalOpen] = useState(false);
+  const [clickedImage, setClickedImage] = useState("");
+  const existingItem = bookmarkedItems.find((item) => item.id === id);
+  const bookmark = useSelector((state) => state.bookmark);
+  const dispatch = useDispatch();
+  console.log(bookmark);
+  const handleBookmark = () => {
+    const bookmarkedItems = JSON.parse(localStorage.getItem("bookmarkedItems")) || [];
+    const existingItem = bookmarkedItems.find((item) => item.id === id);
 
-  const toggleBookmark = (productId) => {
-    if (bookmarkedItems.includes(productId)) {
-      setBookmarkedItems(bookmarkedItems.filter((id) => id !== productId));
+    if (existingItem) {
+      const updatedItems = bookmarkedItems.filter((item) => item.id !== id);
+      localStorage.setItem("bookmarkedItems", JSON.stringify(updatedItems));
+      dispatch(removeFromBookmark(id));
+      console.log("제거되었습니다.");
     } else {
-      setBookmarkedItems([...bookmarkedItems, productId]);
+      const newItem = {
+        id: props.id,
+        title: props.title,
+        brand_image_url: props.brand_image_url,
+        brand_name: props.brand_name,
+        discountPercentage: props.discountPercentage,
+        follower: props.follower,
+        image_url: props.image_url,
+        price: props.price,
+        sub_title: props.sub_title,
+        type: props.type,
+      };
+      bookmarkedItems.push(newItem);
+      localStorage.setItem("bookmarkedItems", JSON.stringify(bookmarkedItems));
+      dispatch(addToBookmark(newItem));
+      console.log("추가되었습니다.");
     }
+  };
+  const handleImageClick = (imageUrl) => {
+    setModalOpen(!modalOpen);
+    setClickedImage(imageUrl);
   };
 
   return (
-    <div>
-      <div className="product">
-        {products.map((product) => (
-          <div key={product.id}>
-            <div className="image-container">
-              <img className="image" alt="제품이미지" src={product.image_url || product.brand_image_url} />
-              <FontAwesomeIcon
-                icon={faStar}
-                onClick={() => toggleBookmark(product.id)}
-                className={`bookmark-icon ${bookmarkedItems.includes(product.id) ? "bookmarked" : ""}`}
-              />
-            </div>
-            <div className="topLine">
-              <div className="leftText">
-                <div className="productTitle">{product.type === "Category" ? "#" + product.title : product.title || product.brand_name}</div>
-              </div>
-              <div className="rightText">
-                {product.type === "Product" ? <span className="discountPercentage">{product.discountPercentage}%</span> : null}
-                {product.type === "Brand" ? <span className="followerCount">관심고객수</span> : null}
-              </div>
-            </div>
-            <div className="priceAndFollower">{product.price || product.follower}</div>
-            <div className="subTitle">{product.sub_title}</div>
-          </div>
-        ))}
+    <div className="item">
+      <div className="whole-wrapper">
+        <div className="image-wrapper">
+          <img
+            className="image"
+            src={props.image_url || props.brand_image_url}
+            alt="대체이미지"
+            onClick={() => handleImageClick(props.image_url || props.brand_image_url)}
+          />
+        </div>
+        <div className="bookmark-wrapper">
+          <button className="bookmark-button" onClick={handleBookmark}>
+            <i className={`fas fa-star ${existingItem ? "custom-star" : ""}`}></i>
+          </button>
+        </div>
       </div>
+      <div className="first-line">
+        <div className="item-title">{props.type === "Brand" ? props.brand_name : props.type === "Category" ? "#" + props.title : props.title}</div>
+        <div className="right-elemnt">
+          {props.type === "Product" ? props.discountPercentage + "%" : null}
+          {props.type === "Brand" ? "관심고객수" : null}
+        </div>
+      </div>
+      <div className="price">{props.price || props.follower}</div>
+      <div>{props.sub_title}</div>
+      {modalOpen && (
+        <div className="modal">
+          <img className="modal-image" src={clickedImage} alt="큰이미지" onClick={handleImageClick} />
+          <button className="modal-bookmark-button" onClick={handleBookmark}>
+            <i className={`fas fa-star ${existingItem ? "custom-star" : ""}`}></i>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
